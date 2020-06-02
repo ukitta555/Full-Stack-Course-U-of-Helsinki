f2c128c703042c0413dea91b21725014ea887644
Vlad Nekriach <ukitta555@gmail.com>
2020-06-02 13:20:30 -0100

    completed exercise 1.7

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -13,19 +13,40 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
-    const handleButtonClick = (value, setValue) => () => setValue(value + 1)
+    const [reviews, setReviews] = useState([])
+    const handleButtonClick = (value, setValue, score) => () => 
+    {
+      setValue(value + 1)
+      setReviews(reviews.concat(score))
+    }
+    const averageScore = () =>
+    {
+      if (reviews.length === 0) return 0
+      else {
+        return reviews.reduce((accum, entry) => accum + entry, 0) / reviews.length
+      }    
+    }
+    const positivePercentage = () =>
+    {
+      if (reviews.length === 0) return 0
+      else {
+        return good / reviews.length
+      }
+    }
 
     return (
       <div>
         <h1> Give feedback </h1>
-        <Button text = "good" handleClick = {handleButtonClick (good, setGood)} />
-        <Button text = "neutral" handleClick = {handleButtonClick (neutral, setNeutral)}/>
-        <Button text = "bad" handleClick = {handleButtonClick (bad, setBad)}/>
+        <Button text = "good" handleClick = {handleButtonClick (good, setGood, 1)} />
+        <Button text = "neutral" handleClick = {handleButtonClick (neutral, setNeutral, 0)}/>
+        <Button text = "bad" handleClick = {handleButtonClick (bad, setBad, -1)}/>
         <h1> Stats </h1>
         <p> good {good} </p>
         <p> neutral {neutral}</p>
         <p> bad {bad} </p>
+        <p> all {good + neutral + bad}</p>
+        <p> average {averageScore()}</p>
+        <p> positive {positivePercentage()*100}%</p>
       </div>
     )
 }
