2e481075b1f655dd7d291daec43142facd1dbc34
Vlad Nekriach <ukitta555@gmail.com>
2020-07-14 20:41:39 -0100

    completed exercise 2.9

--- Part 2/phonebook/src/components/App.js
+++ Part 2/phonebook/src/components/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 import Person from './Person'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      id: 1,
-      phone: '123-123-123'
-    }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1, phone: '040-123456' },
+    { name: 'Ada Lovelace', id: 2, phone: '39-44-5323523' },
+    { name: 'Dan Abramov', id: 3, phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', id: 4, phone: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
-  const [ newPhoneNumber, setNewPhoneNumber] = useState ('')
+  const [ newPhoneNumber, setNewPhoneNumber ] = useState ('')
+  const [ filter, setFilter ] = useState ('')
+  const changeFilter = (event) => {
+    console.log (event.target.value)
+    setFilter(event.target.value)
+  }
   const changeNameInput = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -25,7 +30,7 @@ const App = () => {
         alert (`You haven't filled all the fields yet.`)
     } else if (persons.map
                 (person => person.name.toLocaleLowerCase())
-                .indexOf(newName) != -1)
+                .indexOf(newName) !== -1)
     {
         alert (`${newName} is already present in phonebook`)
     } else 
@@ -41,6 +46,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value = {filter} onChange = {changeFilter}/>
+      </div>
+      <h2>add a new person: </h2>
       <form onSubmit = {addNumber}>
         <div>
           name: <input value = {newName} onChange = {changeNameInput}/>
@@ -54,12 +63,18 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
         <ul>
-            {persons.map(
+            {persons.filter(
+                    (person) => person.name
+                                .toLocaleLowerCase()
+                                .includes(filter.toLocaleLowerCase())
+                    )
+                    .map(
                         (person) => 
                         <Person key = {person.id} 
                                 person = {person} 
-                        />
-            )}
+                        /> 
+                     )
+            }
         </ul>
     </div>
   )
