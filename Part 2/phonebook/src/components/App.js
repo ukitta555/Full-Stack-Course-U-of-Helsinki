4e6f57a03a979ff83542050f522070ce7539c120
Vlad Nekriach <ukitta555@gmail.com>
2020-07-18 17:07:28 -0100

    completed exercise 2.11

--- Part 2/phonebook/src/components/App.js
+++ Part 2/phonebook/src/components/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './Persons'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1, phone: '040-123456' },
-    { name: 'Ada Lovelace', id: 2, phone: '39-44-5323523' },
-    { name: 'Dan Abramov', id: 3, phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', id: 4, phone: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhoneNumber, setNewPhoneNumber ] = useState ('')
   const [ filter, setFilter ] = useState ('')
+
+  useEffect( () => 
+                  { 
+                    axios.get('http://localhost:3001/persons')
+                        .then(response => setPersons(response.data))
+                  } 
+           , [])
+
   const changeFilter = (event) => {
     console.log (event.target.value)
     setFilter(event.target.value)
