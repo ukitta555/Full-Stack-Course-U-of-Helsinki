a985fd7954e3b431ee18bd4002e304fbbd9f83ef
Vlad Nekriach <ukitta555@gmail.com>
2020-07-20 17:28:19 -0100

    completed exercise 2.13

--- Part 2/countries/src/components/App.js
+++ Part 2/countries/src/components/App.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import FindField from "./FindField"
+import CountryPage from "./CountryPage"
+import ButtonToCountryPage from "./ButtonToCountryPage"
 import SearchOptions from "./SearchOptions"
 
 const App = () => 
 {
+  let filteredData = []
+  const [searchResult, setSearchResult] = useState (<></>);
   const [filter, setFilter] = useState ('')
   const [data, setData] = useState ([])
   useEffect (() => {
@@ -14,17 +18,72 @@ const App = () =>
                             setData(response.data)
                           })
                     }, [])
+
+  
+  const generateHandler = index =>
+  {
+    return (() => 
+            {
+              setSearchResult (<CountryPage country = {filteredData[index]}/>)
+              console.log (searchResult)
+            })
+  }
+
   const changeCountryInput = (event) => {
+    
     setFilter (event.target.value)
+    if (event.target.value !== '')
+    { 
+      filteredData = data.filter(country => 
+                                  country.name.toLocaleLowerCase()
+                                              .includes(event.target.value.toLocaleLowerCase())
+                                ) 
+    }
+    if (filteredData.length > 10)
+    {
+      setSearchResult (<p> Too many matches. Specify another filter! </p>)
+    }
+    else if (filteredData.length < 10 && filteredData.length > 1) 
+    {
+      setSearchResult ( <ul> 
+                        {
+                          filteredData.map ((country, index) => 
+                                            {
+                                              return (
+                                                <div key = {country.name}>
+                                                  <li> 
+                                                      {
+                                                      country.name
+                                                      } 
+                                                  </li>
+                                                  <ButtonToCountryPage
+                                                      handleClick = {generateHandler(index)}
+                                                  />
+                                                </div>
+                                              )
+                                            }  
+                                            ) 
+                        }
+                    </ul>)
+    }
+    else if (filteredData.length === 1)
+    {
+      setSearchResult  (<CountryPage country = {filteredData[0]}/>)
+    }
+    else 
+    {
+      setSearchResult(<p></p>)
+    }
   }  
 
   
-  
+
+
   return (
     <div>
        Find countries: 
       <FindField filter = {filter} changeCountryInput = {changeCountryInput}/>
-      <SearchOptions data = {data} filter = {filter}/>
+      <SearchOptions searchResult = {searchResult} />
     </div>
  )
 }
---
+++ Part 2/countries/src/components/ButtonToCountryPage.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ButtonToCountryPage = ({handleClick}) =>
+{
+ return (
+   <button onClick = {handleClick}> show </button>
+ )
+}
+
+export default ButtonToCountryPage
