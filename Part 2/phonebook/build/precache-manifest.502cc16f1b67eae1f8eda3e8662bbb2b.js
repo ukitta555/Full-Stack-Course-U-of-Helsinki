e739576833bdc7761409a61024b338b46dc39500
Vlad Nekriach <ukitta555@gmail.com>
2020-08-06 14:55:05 -0100

    completed exercise 3.10 (frontend build + added proxy to package.json)

---
+++ Part 2/phonebook/build/precache-manifest.502cc16f1b67eae1f8eda3e8662bbb2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862582322940b20588b26456e55720d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096721b997e89d91ed6a",
+    "url": "/static/css/main.b7477cae.chunk.css"
+  },
+  {
+    "revision": "b4d54f1b2e209782bd68",
+    "url": "/static/js/2.809ec15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.809ec15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096721b997e89d91ed6a",
+    "url": "/static/js/main.fbb7a682.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
