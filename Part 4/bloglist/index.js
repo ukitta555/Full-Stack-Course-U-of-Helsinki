ea4d60b168cb4b5cbfc2b7fd06bdf106243ebcd6
Vlad Nekriach <ukitta555@gmail.com>
2020-08-25 11:27:11 -0100

    added router & model for blog

--- Part 4/bloglist/index.js
+++ Part 4/bloglist/index.js
@@ -1,44 +1,20 @@
 const http = require('http')
 const express = require('express')
+const blogsRouter = require ('./controllers/blogs')
 const app = express()
-const cors = require('cors')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 
 app.listen(config.PORT, () => {
